9cc1ba137e6cfeaef965a2fe4ad6b11b75a7b3f0
Lorenzo <lorenzo.orio@icloud.com>
2023-12-11 19:35:29 -0500

    feat: change favicon and modify firebase structure

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,5 +1,7 @@
 import { Inter } from 'next/font/google'
 import './globals.css'
+import { ThemeProvider } from './theme-provider'
+import { ThemeSwitcher } from './components/ThemeSwitcher'
 
 const inter = Inter({ subsets: ['latin'] })
 
@@ -11,7 +13,14 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body
+        className={`${inter.className} bg-slate-50 dark:bg-[#5599ff]`}
+      >
+        <ThemeProvider attribute="class" defaultTheme="system" enableSystem>
+          <ThemeSwitcher />
+          <main>{children}</main>
+        </ThemeProvider>
+      </body>
     </html>
   )
 }
--- src/app/page.js
+++ src/app/page.js
@@ -5,6 +5,21 @@ import { push, get, ref, set } from 'firebase/database'
 import { database } from './firebaseConfig'
 import FluidMeter from './components/FluidMeter'
 
+// Function to extract users for today
+const extractUsersForToday = (records) => {
+  const todayDate = new Date().toISOString().split('T')[0];
+  const adjustedDate = new Date();
+  adjustedDate.setDate(adjustedDate.getDate() - 1);
+  const adjustedDateString = adjustedDate.toISOString().split('T')[0];
+  
+
+  return records[adjustedDateString] || [];
+};
+
+function getSum(user) {
+  return user.data.reduce((sum, entry) => sum + entry.value, 0);
+}
+
 /**
  * Represents the Home component.
  * @returns {JSX.Element} The rendered Home component.
@@ -12,20 +27,26 @@ import FluidMeter from './components/FluidMeter'
 export default function Home() {
   // Define the state variable 'users' and the function to update it 'setUsers'
   const [users, setUsers] = useState([]);
+  const [accumulatedAmount, setAccumulatedAmount] = useState(0);
   
 
   // Fetch the data from the database when the component mounts
   useEffect(() => {
-    const usersRef = ref(database, 'water_consumption');
+    const usersRef = ref(database, 'records');
     get(usersRef)
       .then((snapshot) => {
         if (snapshot.exists()) {
-          // Convert the snapshot data into an array of users
-          const usersArray = Object.entries(snapshot.val()).map(([id, data]) => ({
-            id,
-            ...data,
+          // Extract users for today
+          const todayUsers = extractUsersForToday(snapshot.val());
+
+          // Convert the todayUsers data into an array of users
+          const usersArray = Object.entries(todayUsers).map(([name, data]) => ({
+            name,
+            data: Object.entries(data).map(([time, value]) => ({ time, value })),
           }));
-          // Update the 'users' state with the fetched data
+
+          console.log(usersArray)
+          // Update the 'users' state with the fetched data for today
           setUsers(usersArray);
         } else {
           console.error('No data available from firebase');
@@ -36,49 +57,10 @@ export default function Home() {
       });
   }, []);
 
-  function sumTodayValues(obj) {
-    let sum = 0;
-  
-    // Get yesterday's date in the format "YYYY-MM-DD", because there is a time different in Firebase DB and NextJS current time
-    let yesterdayDate = new Date();
-    yesterdayDate.setDate(yesterdayDate.getDate() - 1);
-    yesterdayDate = yesterdayDate.toISOString().split('T')[0];
-  
-    // Regular expression to match the date part in the keys
-    const dateRegex = /^(\d{4}-\d{2}-\d{2})T/;
-  
-    // Iterate over the properties of the object
-    for (let key in obj) {
-      // Check if the property is not 'id' and is an object
-      if (key !== 'id' && typeof obj[key] === 'object') {
-        // Extract the date using the regular expression
-        const match = key.match(dateRegex);
-  
-        // Check if there is a match and the extracted date is today
-        if (match && match[1] === yesterdayDate) {
-          // Iterate over the properties of the nested object
-          for (let nestedKey in obj[key]) {
-            // Check if the nested object has a numeric value
-            if (obj[key].hasOwnProperty(nestedKey) && typeof obj[key][nestedKey] === 'number') {
-              sum += obj[key][nestedKey];
-            }
-          }
-        }
-      }
-    }
-  
-    return sum;
-  }
-
-  function switchTheme() {
-    
-  } 
-
 
   return (
     <main className="flex min-h-screen flex-col items-center justify-evenly p-4 lg:p-24">
-      <div className="block dark:hidden absolute inset-0 -z-10 h-full w-full items-center px-5 py-24 [background:radial-gradient(125%_125%_at_50%_10%,#000_40%,#63e_100%)]"></div>
-      <button className=''>Switch Theme</button>
+      
       <h1 className="text-4xl font-mono">How much water have you drunk today?</h1>
 
       <div className="mb-4  text-center lg:max-w-5xl lg:w-full lg:mb-0 lg:text-left">
@@ -86,8 +68,8 @@ export default function Home() {
           {users.map((user, index) => (
             <div className="entry mr-8 text-center" key={index}>
               <h1 className="text-2xl md:text-4xl capitalize">{user.id}</h1>
-              <p className='text-sm mb-4'>drank: {sumTodayValues(user)} oz</p>
-              <FluidMeter percentage={sumTodayValues(user)/125 * 100}/>
+              <p className='text-sm mb-4'>drank: {getSum(user)} oz</p>
+              <FluidMeter percentage={getSum(user)/125 * 100}/>
             </div>
           ))}
         </section>
