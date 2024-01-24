f93244ddc7d228d15abcc191c322f72511cd010a
loty2019 <lorenzo.orio@icloud.com>
2024-01-24 14:31:21 -0500

    Change username input to lowercase

--- src/app/page.js
+++ src/app/page.js
@@ -262,7 +262,7 @@ export default function Home() {
              className="bg-gray-50 border mb-1 border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-fit p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
              placeholder="Name"
              value={username}
-             onChange={(e) => setUsername(e.target.value)}
+             onChange={(e) => setUsername(e.target.value.toLowerCase())}
            />
          </label>
          <label htmlFor="objective">
