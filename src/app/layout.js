5445bd40a42b15d081cccf41bd797d10120b6838
loty2019 <lorenzo.orio@icloud.com>
2024-01-26 22:40:53 -0500

    Add SpeedInsights component to RootLayout

--- src/app/layout.js
+++ src/app/layout.js
@@ -19,6 +19,7 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
+      <SpeedInsights/>
       <Head>
         <meta name="viewport" content="width=device-width, initial-scale=1.0, viewport-fit=cover"></meta>
         <meta name="apple-mobile-web-app-capable" content="yes"></meta>
