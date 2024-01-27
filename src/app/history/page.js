0c8b42f07238c455f3c0e0a06c724780d01eb936
loty2019 <lorenzo.orio@icloud.com>
2024-01-26 20:42:54 -0500

    New History Graph, added chart.js and react-chartjs-2 dependencies

---
+++ src/app/history/page.js
@@ -0,0 +1,152 @@
+'use client'
+import {
+    Chart as ChartJS,
+    CategoryScale,
+    LinearScale,
+    PointElement,
+    LineElement,
+    Title,
+    Tooltip,
+    Legend
+  } from 'chart.js';
+  
+  ChartJS.register(
+    CategoryScale,
+    LinearScale,
+    PointElement,
+    LineElement,
+    Title,
+    Tooltip,
+    Legend
+  );
+import React, { useEffect, useState } from 'react';
+import Link from 'next/link';
+import Image from 'next/image';
+import { Line } from 'react-chartjs-2';
+import { get, ref } from 'firebase/database';
+import { database } from '../firebaseConfig';
+import WaterHubLogo from "/public/img/WaterHub.png";
+import Reload from "/public/img/reload.png";
+
+
+export default function Page() {
+    const [chartData, setChartData] = useState({});
+    const [loading, setLoading] = useState(true);
+    const [name , setName] = useState("");
+
+    useEffect(() => {
+        if (typeof window !== 'undefined') {
+            const urlParams = new URLSearchParams(window.location.search);
+            const username = urlParams.get('username');
+            setName(username);
+
+            if (username) {
+                const recordsRef = ref(database, 'records');
+                const objectiveRef = ref(database, `users/${username}/objective`);
+    
+                // Use a Promise to fetch both records and objective in parallel
+                Promise.all([get(recordsRef), get(objectiveRef)]).then(([recordsSnapshot, objectiveSnapshot]) => {
+                    let objective = 0;
+                    if (objectiveSnapshot.exists()) {
+                        objective = objectiveSnapshot.val();
+                    } else {
+                        console.log("No objective data available");
+                    }
+    
+                    if (recordsSnapshot.exists()) {
+                        const records = recordsSnapshot.val();
+                        const userData = {};
+    
+                        // Loop through the records by date
+                        Object.keys(records).forEach((date) => {
+                            if (records[date][username]) {
+                                // Loop through the times for this user on this date
+                                Object.keys(records[date][username]).forEach((time) => {
+                                    const quantity = records[date][username][time];
+                                    // Add the quantity to the existing total for this date
+                                    userData[date] = (userData[date] || 0) + quantity;
+                                });
+                            }
+                        });
+    
+                        // Convert userData to chartData
+                        setChartData({
+                            labels: Object.keys(userData).sort(),
+                            datasets: [{
+                                label: 'Water Consumption',
+                                data: Object.values(userData),
+                                borderColor: 'rgb(75, 192, 192)',
+                                backgroundColor: 'rgba(75, 192, 192, 0.5)',
+                                pointRadius: 4, // Change the size of the points
+                                fill: true,
+                                tension: 0.4,
+
+                            }, {
+                                label: 'Ideal Consumption',
+                                data: Array(Object.keys(userData).length).fill(objective), // Array of 'objective' values
+                                borderColor: 'red',
+                                borderWidth: 3,
+                                borderDash: [10, 5], // line dashed
+                                fill: false,
+                                tension: 0,
+                                pointRadius: 0, // No points along this line
+                            }]
+                        });
+                    } else {
+                        console.log("No records data available");
+                    }
+                    setLoading(false);
+                }).catch((error) => {
+                    console.error("Error fetching data:", error);
+                    setLoading(false);
+                });
+            }
+        }
+    }, []);
+
+    if (loading) {
+        return (
+            <div className="flex flex-col justify-center items-center h-screen">
+                <div className="mb-4">
+                  <Image src={WaterHubLogo} alt="WaterHubLogo" width={100} height={100} className="dark:bg-slate-300 rounded-xl scale-125" />
+                </div>
+                <div role="status" className="scale-150">
+                    <svg aria-hidden="true" className="inline w-8 h-8 text-gray-200 animate-spin dark:text-gray-600 fill-blue-600 dark:fill-gray-300" viewBox="0 0 100 101" fill="none" xmlns="http://www.w3.org/2000/svg">
+                        <path d="M100 50.5908C100 78.2051 77.6142 100.591 50 100.591C22.3858 100.591 0 78.2051 0 50.5908C0 22.9766 22.3858 0.59082 50 0.59082C77.6142 0.59082 100 22.9766 100 50.5908ZM9.08144 50.5908C9.08144 73.1895 27.4013 91.5094 50 91.5094C72.5987 91.5094 90.9186 73.1895 90.9186 50.5908C90.9186 27.9921 72.5987 9.67226 50 9.67226C27.4013 9.67226 9.08144 27.9921 9.08144 50.5908Z" fill="currentColor"/>
+                        <path d="M93.9676 39.0409C96.393 38.4038 97.8624 35.9116 97.0079 33.5539C95.2932 28.8227 92.871 24.3692 89.8167 20.348C85.8452 15.1192 80.8826 10.7238 75.2124 7.41289C69.5422 4.10194 63.2754 1.94025 56.7698 1.05124C51.7666 0.367541 46.6976 0.446843 41.7345 1.27873C39.2613 1.69328 37.813 4.19778 38.4501 6.62326C39.0873 9.04874 41.5694 10.4717 44.0505 10.1071C47.8511 9.54855 51.7191 9.52689 55.5402 10.0491C60.8642 10.7766 65.9928 12.5457 70.6331 15.2552C75.2735 17.9648 79.3347 21.5619 82.5849 25.841C84.9175 28.9121 86.7997 32.2913 88.1811 35.8758C89.083 38.2158 91.5421 39.6781 93.9676 39.0409Z" fill="currentFill"/>
+                    </svg>
+                </div>
+            </div>
+        );
+    }
+
+    return (
+      <main>
+        <div className="absolute p-0 left-4  top-4 flex flex-row-reverse items-center justify-center">
+            <Link href="./">
+              <button className="bg-slate-600 p-2 font-sans font-bold text-slate-200 rounded-lg hover:scale-110 active:scale-100 duration-200">← Go Back</button>
+            </Link>
+        </div>
+        <div className="absolute p-0 right-16 top-3 flex flex-row-reverse items-center justify-center scale-125 hover:scale-150 duration-200">
+          <Image 
+            src={Reload} 
+            alt="settings" 
+            width={32} height={32} 
+            className="bg-slate-600 mr- p-1.5 rounded-md hover:ease-in-out duration-300"
+            onClick={() => {window.location.reload(true)}}
+          />
+        </div>    
+        <div className="flex flex-col items-center mt-10 p-2 justify-center">
+            <Image src={WaterHubLogo} alt="WaterHubLogo" width={100} height={100} className="dark:bg-slate-300 rounded-xl" />
+            <h1 className="text-4xl mb-4 font-sans font-bold lg:mt-0 text-blue-950 dark:text-white">{String(name).charAt(0).toUpperCase() + String(name).slice(1)}&apos;s History</h1>
+        </div>
+        <div className='flex justify-center items-center'>
+            {chartData && chartData.labels ? (
+                <Line data={chartData} options={{ responsive: true }} className='bg-[#ffffff60] backdrop-blur-md rounded-2xl m-4' />
+            ) : (
+                <p>No data to display</p>
+            )}
+        </div>
+      </main>
+    );
+}
--- src/app/page.js
+++ src/app/page.js
@@ -10,6 +10,7 @@ import WaterHubLogo from "/public/img/WaterHub.png";
 import Settings from "/public/img/settingImg.png";
 import Reload from "/public/img/reload.png";
 import NewUser from "/public/img/newUser.png";
+import History from "/public/img/history.png";
 
 // Function to extract users for today
 const extractUsersForToday = (records) => {
@@ -58,6 +59,7 @@ export default function Home() {
   const [expanded, setExpanded] = useState(false);
   const [inputValues, setInputValues] = useState({});
   const [submitDone, setSubmitDone] = useState({});
+  const [loading, setLoading] = useState(true);
   const [confirmationName, setConfirmationName] = useState({});
 
 
@@ -169,9 +171,11 @@ export default function Home() {
         } else {
           console.error("No data available from firebase");
         }
+        setLoading(false);
       })
       .catch((error) => {
         console.error(error);
+        setLoading(false);
       });
   }, []);
 
@@ -217,6 +221,22 @@ export default function Home() {
     }
   };
 
+  if (loading) {
+    return (
+        <div className="flex flex-col justify-center items-center h-screen">
+            <div className="mb-4">
+              <Image src={WaterHubLogo} alt="WaterHubLogo" width={100} height={100} className="dark:bg-slate-300 rounded-xl scale-125" />
+            </div>
+            <div role="status" className="scale-150">
+                <svg aria-hidden="true" className="inline w-8 h-8 text-gray-200 animate-spin dark:text-gray-600 fill-blue-600 dark:fill-gray-300" viewBox="0 0 100 101" fill="none" xmlns="http://www.w3.org/2000/svg">
+                    <path d="M100 50.5908C100 78.2051 77.6142 100.591 50 100.591C22.3858 100.591 0 78.2051 0 50.5908C0 22.9766 22.3858 0.59082 50 0.59082C77.6142 0.59082 100 22.9766 100 50.5908ZM9.08144 50.5908C9.08144 73.1895 27.4013 91.5094 50 91.5094C72.5987 91.5094 90.9186 73.1895 90.9186 50.5908C90.9186 27.9921 72.5987 9.67226 50 9.67226C27.4013 9.67226 9.08144 27.9921 9.08144 50.5908Z" fill="currentColor"/>
+                    <path d="M93.9676 39.0409C96.393 38.4038 97.8624 35.9116 97.0079 33.5539C95.2932 28.8227 92.871 24.3692 89.8167 20.348C85.8452 15.1192 80.8826 10.7238 75.2124 7.41289C69.5422 4.10194 63.2754 1.94025 56.7698 1.05124C51.7666 0.367541 46.6976 0.446843 41.7345 1.27873C39.2613 1.69328 37.813 4.19778 38.4501 6.62326C39.0873 9.04874 41.5694 10.4717 44.0505 10.1071C47.8511 9.54855 51.7191 9.52689 55.5402 10.0491C60.8642 10.7766 65.9928 12.5457 70.6331 15.2552C75.2735 17.9648 79.3347 21.5619 82.5849 25.841C84.9175 28.9121 86.7997 32.2913 88.1811 35.8758C89.083 38.2158 91.5421 39.6781 93.9676 39.0409Z" fill="currentFill"/>
+                </svg>
+            </div>
+        </div>
+    );
+  }
+
   return (
     <main className=" min-h-screen p-4 lg:p-14 flex-col">
       <div className="absolute p-0 right-16 top-3 flex flex-row-reverse items-center justify-center">
@@ -231,13 +251,13 @@ export default function Home() {
             />
           </Link>
         </div>
-        <div className="scale-125 hover:scale-150 duration-200  cursor-wait">
+        <div className="mr-5">
           <Image
             src={Reload}
             alt="Reload"
             width={32}
             height={32}
-            className="bg-slate-600 mr-5 p-1.5 rounded-md"
+            className="bg-slate-600 p-1.5 rounded-md scale-125 hover:scale-150 duration-200"
             onClick={() => {
               window.location.reload(true);
             }}
@@ -273,9 +293,7 @@ export default function Home() {
         </h1>
       </div>
       <div className="mb-4 p-5 text-center backdrop-blur-sm bg-white/10 lg:max-w-5xl lg:w-full lg:mb-0 lg:text-left mx-auto rounded-3xl">
-        <section
-          className={`grid grid-cols-2 md:grid-cols-3 justify-evenly`}
-        >
+        <section className={`grid grid-cols-2 md:grid-cols-3 justify-evenly`}>
           {users.map((user, index) => (
             <div className="entry flex-row text-center mb-16" key={index}>
               <h1 className="text-2xl font-bold md:text-4xl capitalize">
@@ -293,30 +311,28 @@ export default function Home() {
               <FluidMeter
                 percentage={(getSum(user) / (objectives[user.name] + 10)) * 100}
               />
-              
-              <Link href={`./fillMenu?username=${user.name}`}>
-                <div className="flex items-baseline justify-center">
-                  <button
-                    className="mt-4 z-10 text-lg border-2 border-black bg-[#55c0F3]  text-white dark:border-white text-center hover:scale-125 font-extrabold py-1 px-2 rounded-xl transition-all duration-200 "
-                    //onFocus={() => setFocusStates({ ...focusStates, [user.name]: true })}
-                    //onBlur={() => handleBlur(user.name)} // this is causing the button to disappear when clicked
-                  >
-                    More +
-                  </button>
-                  <div className="absolute mt-5 w-14 z-0 h-8 bg-[#2a7892] rounded-xl transition-all duration-200 animate-ping " />
-                </div>
-              </Link>
 
-              {submitDone[user.name] && (
-                <div className="flex justify-center">
-                  <p className="text-lg pt-5 font-semibold text-blue-950 dark:text-white">
-                    <span className="font-extrabold">{confirmationName}</span>{" "}
-                    added! 👍
-                    <br />
-                    Keep Drinking!
-                  </p>
+              <div className="flex items-center justify-center space-x-4">
+                <Link href={`./fillMenu?username=${user.name}`}>
+                  <div className="flex items-baseline justify-center">
+                    <button className="mt-4 z-10 text-lg border-2 border-[#000000b8] bg-[#55c0F3] text-white dark:border-white text-center hover:scale-125 font-extrabold py-1 px-2 rounded-xl transition-all duration-200">
+                      More +
+                    </button>
+                    <div className="absolute mt-5 w-14 z-0 h-8 bg-[#2a7892] rounded-xl transition-all duration-200 animate-ping" />
+                  </div>
+                </Link>
+                <div className="scale-110 hover:scale-150 duration-200 cursor-pointer">
+                  <Link href={`./history?username=${user.name}`}>
+                    <Image
+                      src={History}
+                      alt="history"
+                      width={33}
+                      height={30}
+                      className="bg-[#55c0F3] border-[#000000b8] border-2 p-1 rounded-lg mt-4"
+                    />
+                  </Link>
                 </div>
-              )}
+              </div>
             </div>
           ))}
         </section>
