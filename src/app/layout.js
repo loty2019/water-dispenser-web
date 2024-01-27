90aa75a93f1da090356be0f1a4ab86b53546ab2b
loty2019 <lorenzo.orio@icloud.com>
2024-01-26 22:55:10 -0500

    SpeedInsights

--- src/app/layout.js
+++ src/app/layout.js
@@ -19,7 +19,6 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <SpeedInsights/>
       <Head>
         <meta name="viewport" content="width=device-width, initial-scale=1.0, viewport-fit=cover"></meta>
         <meta name="apple-mobile-web-app-capable" content="yes"></meta>
@@ -27,9 +26,8 @@ export default function RootLayout({ children }) {
         <link rel="manifest" href="/manifest.json" />
         <link rel="apple-touch-icon" href="/logo/512.png"></link>
       </Head>
-      <body
-        className={`${inter.className}`}
-      >
+      <body className={`${inter.className}`} >
+        <SpeedInsights/>
         <ThemeProvider attribute="class" defaultTheme="light" enableSystem>
           <canvas
             id="gradient-canvas"
@@ -37,7 +35,7 @@ export default function RootLayout({ children }) {
             data-transition-in
           />
           <ThemeSwitcher />
-          <main>{children}</main>
+          <main>{children} <SpeedInsights/></main>
         </ThemeProvider>
       </body>
     </html>
