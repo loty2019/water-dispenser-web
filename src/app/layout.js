247a6360a746afb86623c7d2102eb9c2ae8cc5bd
loty2019 <lorenzo.orio@icloud.com>
2024-01-26 22:21:28 -0500

    Iphone layout

--- src/app/layout.js
+++ src/app/layout.js
@@ -19,8 +19,9 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <Head>
-        <meta name="viewport" content="initial-scale=1, viewport-fit=cover"></meta>
+        <meta name="viewport" content="width=device-width, initial-scale=1.0, viewport-fit=cover"></meta>
         <meta name="apple-mobile-web-app-capable" content="yes"></meta>
+        <meta name="apple-mobile-web-app-status-bar-style" content="black-translucent"></meta>
         <link rel="manifest" href="/manifest.json" />
         <link rel="apple-touch-icon" href="/logo/512.png"></link>
       </Head>
